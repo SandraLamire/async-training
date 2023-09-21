624d20b3a840f0ff2b42b17d377b528332c978ab
SandraLamire <sandra.lamire.sl@gmail.com>
2023-09-21 11:47:01 +0200

    TD Code Morse - Callback

--- main.js
+++ main.js
@@ -21,9 +21,9 @@ const callbackLog1s = () => {
 wait1sWithCallback(callbackLog1s);
 console.log('CALLBACK : Before callback');
 
+/* PROMISE */
 const echec = false;
 
-/* PROMISE */
 const wait1sWithPromise = () => new Promise((resolve, reject) => {
     setTimeout(() => {
         if (echec) {
@@ -69,3 +69,84 @@ try {
 
 /*********************** TRAINING ********************************/
 /* TD M07D01 - Code Morse */
+/* Ecrire dans la console SOS en morse (3 bips longs, 3 bips courts, 3 bips longs) de 3 manières différentes */
+
+/* CALLBACK */
+/*
+class MorseCode {
+    sosCallback() {
+      this.bipCourt(() => {
+        this.bipCourt(() => {
+          this.bipCourt(() => {
+            this.bipLong(() => {
+              this.bipLong(() => {
+                this.bipLong(() => {
+                  this.bipCourt(() => {
+                    this.bipCourt(() => {
+                      this.bipCourt(() => {
+                        console.log("STOP");
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    }
+  
+    bipCourt(callback) {
+      console.log(".");
+      setTimeout(callback, 100);
+    }
+  
+    bipLong(callback) {
+      console.log("-");
+      setTimeout(callback, 500);
+    }
+  }
+  
+  const morseCodeInstance = new MorseCode();
+  morseCodeInstance.sosCallback();
+*/
+
+/* OU */
+function sosCallback() {
+  function bipCourt(callback) {
+    // jouer un bip sonore court d'une dixième de seconde
+    console.log("Bip court");
+    setTimeout(callback, 100);
+  }
+
+  function bipLong(callback) {
+    // jouer un bip sonore long d'une seconde
+    console.log("Bip long");
+    setTimeout(callback, 1000);
+  }
+
+  bipCourt(() => {
+    bipCourt(() => {
+      bipCourt(() => {
+        bipLong(() => {
+          bipLong(() => {
+            bipLong(() => {
+              bipCourt(() => {
+                bipCourt(() => {
+                  bipCourt(() => {
+                    console.log("STOP");
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+}
+
+sosCallback();
+
+
+
